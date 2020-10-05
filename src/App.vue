73acfb18f4db4958294f6f069f006787c9029999
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-10-05 16:38:45 +0200

    Step 4

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
     <hr />
     <Step3 />
     <hr />
-    <Step4 />
+    <h3 title="step 4">{{ title4 }}</h3>
+    <Step4 v-for="person in persons" :key="person.id" :person="person" />
   </div>
 </template>
 
@@ -19,6 +20,55 @@ import Step4 from "./components/Step4.vue";
 export default {
   name: "App",
   components: { Step1, Step2, Step3, Step4 },
+  data() {
+    return {
+      title4: "step 4",
+      persons: [
+        {
+          id: 1,
+          title: "Alejandra Sanchez",
+          description: {
+            Firstname: "Alejandra",
+            Lastname: "Sanchez",
+            Email: "alejandrasanchez@ono.es",
+            Username: "alejandras",
+            Password: "mOrUzuih",
+            Dateofbirth: "1983-04-08",
+            Age: 37,
+            Sex: "female",
+          },
+        },
+        {
+          id: 2,
+          title: "Ekhardt H. Nehls",
+          description: {
+            Firstname: "Ekhardt",
+            Lastname: "Nehls",
+            Email: "ekhardtnehls@yahoo.com",
+            Username: "ekhardtn",
+            Password: "1OhabIYp",
+            Dateofbirth: "1990-06-05",
+            Age: 30,
+            Sex: "male",
+          },
+        },
+        {
+          id: 3,
+          title: "Sebastian L. Walton",
+          description: {
+            Firstname: "Sebastian",
+            Lastname: "Walton",
+            Email: "sebastianwalton@gmail.com",
+            Username: "swalton",
+            Password: "kgzAiXRf",
+            Dateofbirth: "1961-04-09",
+            Age: 59,
+            Sex: "male",
+          },
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- src/components/Step4.vue
+++ src/components/Step4.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="container">
-    <h3 title="title">{{ title }}</h3>
+    <p>
+      <strong
+        ><i>{{ person.title }}</i></strong
+      >
+    </p>
+    <p @click="toggle = !toggle" class="btn btn-primary">personal details</p>
+    <div v-if="toggle === true" class="mb-3">
+      <strong>First name:</strong> {{ person.description.Firstname }} --
+      <strong>Last name:</strong>
+      {{ person.description.Lastname }}
+      <br />
+      <strong>Username:</strong> {{ person.description.Username }}
+      <br />
+      <strong>Email:</strong> {{ person.description.Email }}
+      <br />
+      <strong>Date of birth:</strong> {{ person.description.Dateofbirth }} --
+      <strong>Age:</strong>
+      {{ person.description.Age }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Step4",
+  props: ["person"],
   data() {
     return {
-      title: "step 4",
+      toggle: false,
     };
   },
 };
