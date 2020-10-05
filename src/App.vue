2025254da37042e9e5b3d270d9d80e48ae6a636c
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-10-05 14:43:08 +0200

    Step 3

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <Step1 />
     <hr />
     <Step2 />
+    <hr />
+    <Step3 />
   </div>
 </template>
 
 <script>
 import Step1 from "./components/Step1.vue";
 import Step2 from "./components/Step2.vue";
+import Step3 from "./components/Step3.vue";
 
 export default {
   name: "App",
-  components: { Step1, Step2 },
+  components: { Step1, Step2, Step3 },
 };
 </script>
 
---
+++ src/components/Step3.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h3 title="title">{{ title }}</h3>
+
+    <input type="number" v-model="price" />
+    <div>Taxe {{ taxFormatted }}</div>
+    <div>Total {{ totalFormatted }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Step3",
+  data() {
+    return {
+      title: "step 3",
+      price: 100,
+    };
+  },
+  computed: {
+    tax() {
+      return this.price * 0.1;
+    },
+    taxFormatted() {
+      return "$" + this.tax;
+    },
+    total() {
+      return parseInt(this.price) + this.tax;
+    },
+    totalFormatted() {
+      return "$" + this.total;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
