ddf2f77972bcebef4b8dded68d43f73aef4e8af8
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-10-05 14:20:27 +0200

    Step 2

--- src/components/Step2.vue
+++ src/components/Step2.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-    <h3 title="title">{{ title }}</h3>
+    <h3 v-if="!hidden" title="title">{{ title }}</h3>
+    <p v-else>step 999999</p>
+
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item.text }} -
+        <div v-if="item.checked">Checked!</div>
+
+        <button @click="item.checked = !item.checked" class="btn btn-primary">
+          toggle
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +22,21 @@ export default {
   data() {
     return {
       title: "step 2",
+      hidden: false,
+      items: [
+        {
+          text: "item1",
+          checked: true,
+        },
+        {
+          text: "item2",
+          checked: true,
+        },
+        {
+          text: "item3",
+          checked: false,
+        },
+      ],
     };
   },
 };
